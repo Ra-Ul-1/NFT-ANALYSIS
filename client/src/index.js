e4292ef425fa851de30c84902a2ed1681a98eb98
Raoul Ullens <raoulullens@live.com>
2021-07-27 15:08:04 +0200

    add first part of authentication - with bcrypt bug"

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+
+// get the user from the backend and pass it to App.js as a prop
+axios.get('/api/auth/loggedin')
+  .then(response => {
+    console.log('logged in user: ', response.data);
+    const user = response.data;
+    ReactDOM.render(
+      <BrowserRouter>
+      {/* passing it to app,js as a prop - i.e. user */}
+        <App user={user} />
+      </BrowserRouter>,
+      document.getElementById('root')
+    );
+  })
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
