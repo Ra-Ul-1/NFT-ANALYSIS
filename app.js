2a4ccccf6575a3e3e5b4718dc40d48ac3b2bca16
Raoul Ullens <raoulullens@live.com>
2021-07-29 20:27:57 +0200

    try change DB_URL in app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
-
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -19,7 +18,7 @@ require("./config")(app);
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const DB_URL = 'mongodb://localhost/projector';
+const DB_URL = process.env.MONGODB_URI;
 
 
 app.use(
@@ -103,6 +102,13 @@ app.use("/", marketOverview);
 const auth = require("./routes/auth");
 app.use("/api/auth", auth);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/client/build/index.html");
+  });
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
